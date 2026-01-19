ce0ad481ff5ea4f2f4b586bbbd9f0a7565348f95
Pradeep kumar <67779930+Pradeep76339@users.noreply.github.com>
2026-01-19 16:56:07 +0530

    Update lab.sh

--- Google Kubernetes Engine: Qwik Start/lab.sh
+++ Google Kubernetes Engine: Qwik Start/lab.sh
@@ -5,17 +5,25 @@
 
 ```bash
 gcloud config set compute/region us-central1
+
 gcloud config set compute/zone us-central1-b
+
 gcloud container clusters create --machine-type=e2-medium --zone=us-central1-b lab-cluster
+
 gcloud container clusters get-credentials lab-cluster
+
 kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:1.0
+
+
 kubectl expose deployment hello-server --type=LoadBalancer --port 8080
 kubectl get service
+
 ...
 
 ...bash
 #get EXTERNAL-IP and paste
 http://[EXTERNAL-IP]:8080
+
 gcloud container clusters delete lab-cluster
 ...
 </div>
